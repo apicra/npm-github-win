5fb14adb2a6bdea13291071447ca9698f0341d6a
tom <tom@sapletta.com>
2019-05-23 12:57:56 +0200

    version changing

--- -ticket.bat
+++ -ticket.bat
@@ -5,6 +5,9 @@ set /p VERSION= < .apicra\.variable\VERSION.txt
 IF "%CONTENT%" == "" set CONTENT=New Version of System v%VERSION%
 set ticket=".apicra\.ticket\v%VERSION%.txt"
 echo %ticket%
-IF not exist %ticket% touch %ticket% || exit
-::echo %CONTENT% > %ticket%
-echo %CONTENT% >> %ticket%
+IF not exist %ticket% (
+::touch %ticket%
+    echo %CONTENT% > %ticket%
+) ElSE (
+    echo %CONTENT% >> %ticket%
+)
