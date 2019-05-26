1fddfb8759f3646b56b885d766e58030094f7214
tom <tom@sapletta.com>
2019-05-26 19:28:06 +0200

    Variable, preparing for test

--- -variable-home-url.bat
+++ -variable-home-url.bat
@@ -1,1 +1,1 @@
-npm version patch && node -pe "require('../package.json').homepage" > .apicra\variable\HOME_URL.txt
+npm version patch && node -pe "require('./package.json').homepage" > .apicra\variable\HOME_URL.txt
