2f875b74bb721acf4ce171d2c13fda36010989fa
tom <tom@sapletta.com>
2019-05-26 19:53:03 +0200

    Variable, preparing for test

--- -github-push.bat
+++ -github-push.bat
@@ -5,5 +5,5 @@ SetLocal EnableDelayedExpansion
 for /f %%i in ('dir /b /od ticket\*.txt') do set filename=%%i
 for /f "delims==" %%a in (ticket\%filename%) do set message=%%a
 @ECHO ON
-git add . && git commit -m "%message%" && git push origin master && start firefox "https://github.com/gitpad-pl/github.git"
+git add . && git commit -m "%message%" && git push origin master && -open-git-url.bat
 :: GIT_URL.txt
---
+++ -open-git-url.bat
@@ -0,0 +1,2 @@
+set /P GIT_URL= < .apicra\variable\GIT_URL.txt
+start firefox GIT_URL
