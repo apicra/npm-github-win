612c23d8bebeb0bcdb376afed5b84be2852ab31d
tom <tom@sapletta.com>
2019-05-28 11:06:34 +0200

    scripts for creating one-file-modules, curl library, support creating repo direct on github

--- -git-create.bat
+++ -git-create.bat
@@ -1,9 +1,6 @@
 :: create git repo
 :: cd project folder
-git init
-git add .
-::git checkout -t -b develop origin/develop
-git commit -m "initial project version"
+
 ::# Verifies the new remote URL
 ::git remote -v
 ::curl -u 'tom-sapletta-com' https://api.github.com/user/repos -d "{\"name\":\"%PROJECT%\"}"
@@ -13,14 +10,13 @@ set PROJECT=%~2
 set /P DESCRIPTION= < ..\.apicra\variable\DESCRIPTION.txt
 ::set GIT_USER=tom-sapletta-com
 ..\.apicra\curl-7.65.0-win32-mingw\bin\curl.exe -u "%GIT_USER%" https://api.github.com/user/repos -d "{\"name\":\"%PROJECT%\", \"description\":\"%DESCRIPTION%\"}"
-
-
+git init
+git add .
+::git checkout -t -b develop origin/develop
+git commit -m "initial project version"
+::# Sets the new remote
+git remote add origin https://github.com/%GIT_USER%/%PROJECT%.git
 ::# Pushes the changes in your local repository up to the remote repository you specified as the origin
 git push -u origin master
-
-::# Sets the new remote
-git remote add origin "https://github.com/%GIT_USER%/%PROJECT%.git"
-
-
 :: https://curl.haxx.se/windows/
 :: https://curl.haxx.se/windows/dl-7.65.0/curl-7.65.0-win32-mingw.zip
