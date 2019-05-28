17725a00ccb66d2bfd78fe9bfea7b69d5801e34b
tom <tom@sapletta.com>
2019-05-28 11:10:04 +0200

    scripts for creating one-file-modules, curl library, support creating repo direct on github

--- -project-create.bat
+++ -project-create.bat
@@ -2,10 +2,12 @@
 :: @ECHO OFF
 set GIT_USER=%~1
 set PROJECT=%~2
+set /P DESCRIPTION= < ..\.apicra\variable\DESCRIPTION.txt
 mkdir %PROJECT%
 cd %PROJECT%
 ::@ECHO ON
 echo # %PROJECT% > README.md
+echo %DESCRIPTION% >> README.md
 echo # Licence > LICENCE.md
 ..\.apicra\-git-create.bat %GIT_USER% %PROJECT%
 echo Project: "%PROJECT%" is Created
