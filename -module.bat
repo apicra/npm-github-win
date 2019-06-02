38da8a1cf020cb486ec068a1a418fb8db95f308d
tom <tom@sapletta.com>
2019-06-02 16:14:04 +0200

    project function

--- -module.bat
+++ -module.bat
@@ -54,19 +54,19 @@ IF EXIST ".apicra\module\%MODULE%.txt" (
 GOTO end
 ::::::::::::::
 :install
-echo ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo ::
 echo :: Install module :: %MODULE%
-echo ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo ::
 if "%MODULE%"=="" GOTO model_empty
 ::mkdir .apicra
 ::echo model/%MODULE%/ >> .gitignore
 git clone https://github.com/apicra/%OS%-%MODULE%.git .apicra\module\%MODULE% && echo :: %MODULE% is installed
 :: Create config file
 echo "" > %VARIABLE_PATH%
-IF EXIST %VARIABLE_PATH% echo Variable for %MODULE% is created
+IF EXIST %VARIABLE_PATH% echo :: Variable for %MODULE% is created
 ::install_module_from_config
 IF EXIST %APICRA_CONFIG% (
-    ECHO Install All modules from config file %APICRA_CONFIG%
+    ECHO :: Install All modules from config file %APICRA_CONFIG%
     GOTO install_module_from_config
 ) ELSE (
     GOTO end
@@ -77,9 +77,9 @@ for /f "delims==" %%a in (%APICRA_CONFIG%) do .apicra\-module.bat install %%a
 GOTO end
 ::::::::::::::
 :update
-echo ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo ::
 echo :: Update module :: %MODULE%
-echo ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo ::
 if "%MODULE%"=="" GOTO model_empty
 ::mkdir .apicra
 ::echo model/%MODULE%/ >> .gitignore
@@ -87,9 +87,9 @@ git -C .apicra\module\%MODULE% pull origin master && echo :: %MODULE% is install
 GOTO end
 ::::::::::::::
 :delete
-echo ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo ::
 echo :: Delete module :: %MODULE%
-echo ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo ::
 if "%MODULE%"=="" GOTO model_empty
 IF NOT EXIST %MODULE_PATH% GOTO model_not_exist
 RMDIR /Q /S .apicra\module\%MODULE% && echo :: %MODULE% module folder is deleted
